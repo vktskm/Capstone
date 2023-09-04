f6a4d4485f01828280af4d9d1236fb14fc4103b3
vktskm <vinkentskembri@gmail.com>
2023-09-04 10:56:58 +0200

    primo commit

---
+++ Sicilia/src/main/java/com/example/sicilia/SiciliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sicilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiciliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiciliaApplication.class, args);
+	}
+
+}
---
+++ Sicilia/src/test/java/com/example/sicilia/SiciliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sicilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiciliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
